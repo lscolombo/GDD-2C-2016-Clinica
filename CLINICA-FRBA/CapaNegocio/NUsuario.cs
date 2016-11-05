eed676a696fdc8a6d44c39c984506fc52e0193ed
Germán Guzmán <guzz89@hotmail.com>
2016-11-05 15:08:19 -0300

    Login en progreso

---
+++ CLINICA-FRBA/CapaNegocio/NUsuario.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class NUsuario
+    {
+        //Métodos para comunicarnos con la capa datos
+        //Método Insertar que llama al método Insertar de la clase DUsuario
+        //de la CapaDatos
+        public static string Insertar(string user_username, string user_nombre, string user_apellido,
+                        string user_telefono, char user_sexo, string user_estado_civil,
+                        string user_dni, string user_tipo_dni, DateTime user_fecha_nac,
+                        string user_direccion, string user_mail, string user_password,
+                        int user_intentos_fallidos, bool user_habilitado)
+        {
+            DUsuario Obj = new DUsuario();
+            Obj.User_username = user_username;
+            Obj.User_nombre = user_nombre;
+            Obj.User_apellido = user_apellido;
+            Obj.User_telefono = user_telefono;
+            Obj.User_sexo = user_sexo;
+            Obj.User_estado_civil = user_estado_civil;
+            Obj.User_dni = user_dni;
+            Obj.User_tipo_dni = user_tipo_dni;
+            Obj.User_fecha_nac = user_fecha_nac;
+            Obj.User_direccion = user_direccion;
+            Obj.User_mail = user_mail;
+            Obj.User_password = user_password;
+            Obj.User_intentos_fallidos = user_intentos_fallidos;
+            Obj.User_habilitado = user_habilitado;
+
+            return Obj.Insertar(Obj);
+        }
+
+        //Método Editar que llama al método Editar de la clase DUsuario
+        //de la CapaDatos
+        public static string Editar(string user_username, string user_nombre, string user_apellido,
+                        string user_telefono, char user_sexo, string user_estado_civil,
+                        string user_direccion, string user_mail, string user_password,
+                        bool user_habilitado)
+        {
+            DUsuario Obj = new DUsuario();
+            Obj.User_username = user_username;
+            Obj.User_nombre = user_nombre;
+            Obj.User_apellido = user_apellido;
+            Obj.User_telefono = user_telefono;
+            Obj.User_sexo = user_sexo;
+            Obj.User_estado_civil = user_estado_civil;
+            Obj.User_direccion = user_direccion;
+            Obj.User_mail = user_mail;
+            Obj.User_password = user_password;
+            Obj.User_habilitado = user_habilitado;
+
+            return Obj.Editar(Obj);
+        }
+
+        //Método Eliminar que llama al método Eliminar de la clase DUsuario
+        //de la CapaDatos
+        public static string Eliminar(string user_username)
+        {
+            DUsuario Obj = new DUsuario();
+            Obj.User_username = user_username;
+            return Obj.Eliminar(Obj);
+        }
+
+        public static DataTable Login(string user_username, string user_password)
+        {
+            DUsuario Obj = new DUsuario();
+            Obj.User_username = user_username;
+            Obj.User_password = user_password;
+            return Obj.Login(Obj);
+        }
+    }
+}
---
+++ CLINICA-FRBA/CapaPresentacion/frmLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            DataTable Datos = new DataTable();
+            Datos = CapaNegocio.NUsuario.Login(this.TxtUsuario.Text, this.TxtPassword.Text);
+            //Evaluar si existe el Usuario
+            if (Datos.Rows.Count == 0)
+            {
+                MessageBox.Show("NO Tiene Acceso al Sistema", "Clinica FRBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                /*
+                frmPrincipal frm = new frmPrincipal();
+                frm.Idtrabajador = Datos.Rows[0][0].ToString();
+                frm.Apellidos = Datos.Rows[0][1].ToString();
+                frm.Nombre = Datos.Rows[0][2].ToString();
+                frm.Acceso = Datos.Rows[0][3].ToString();
+
+                frm.Show();
+                this.Hide();
+                */
+
+                MessageBox.Show("SI Tiene Acceso al Sistema", "Clinica FRBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+    }
+}
